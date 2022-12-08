62245d385b1e67fa012fb3513fdd5a621b19222e
leesj-dev <104193215+leesj-dev@users.noreply.github.com>
2022-12-08 23:00:30 +0900

    lv3 일부 완료

---
+++ lv3/10950.py
@@ -0,0 +1,4 @@
+lst = []
+for i in range(0, int(input())):
+    lst.append(str(sum(map(int, input().split(" ")))))
+print("\n".join(lst))
---
+++ lv3/8393.py
@@ -0,0 +1,4 @@
+sum = 0
+for i in range(0, int(input())):
+    sum += i + 1
+print(sum)
